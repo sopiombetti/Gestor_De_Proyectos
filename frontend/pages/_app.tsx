6792156d69c1006ffc939b8ed5723f64548cd492
sopiombetti <sopiombetti@gmail.com>
2026-04-27 14:25:55 -0300

    Instalando tailwind css

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
