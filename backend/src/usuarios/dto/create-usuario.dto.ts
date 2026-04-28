9fd7a6c7f337c050a51ab513184ab337dd3dd89e
Minera José Luis <minerajl@debian>
2026-04-28 00:03:07 -0300

    Se añade DTO para el create

--- backend/src/usuarios/dto/create-usuario.dto.ts
+++ backend/src/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUsuarioDto {}
+export class CreateUsuarioDto {
+    nombre!: string;
+    apellido!: string;
+    email!: string;
+    password!: string;
+    posicion_laboral!: string;
+}
