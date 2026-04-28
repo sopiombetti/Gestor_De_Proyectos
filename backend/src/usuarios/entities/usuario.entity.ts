e42ed9b50ce1521bf6ebd5f353a6b261d92edccd
Minera José Luis <minerajl@debian>
2026-04-28 00:00:56 -0300

    Se añade entidad usuario

--- backend/src/usuarios/entities/usuario.entity.ts
+++ backend/src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,31 @@
-export class Usuario {}
+import { Proyecto } from 'src/proyectos/entities/proyecto.entity';
+import { Tarea } from 'src/tareas/entities/tarea.entity';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+
+@Entity('Usuarios')
+export class Usuario {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    nombre!: string;
+
+    @Column()
+    apellido!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    password!: string;
+
+    @Column()
+    posicion_laboral!: string;
+
+    @OneToMany(() => Tarea, (tarea) => tarea.usuario)
+    tareas!: Tarea[];
+
+    @OneToMany(() => Proyecto, (proyecto) => proyecto.lider)
+    proyectos!: Proyecto[];
+
+}
