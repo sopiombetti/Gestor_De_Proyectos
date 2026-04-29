4485d5688157e30c5f3d6844b15efcd9a11e0dad
Minera José Luis <minerajl@debian>
2026-04-28 21:57:36 -0300

    Se añade conexión a bbdd

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { TareasModule } from './tareas/tareas.module';
 import { ProyectosModule } from './proyectos/proyectos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsuariosModule, TareasModule, ProyectosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'gestor_proyectos',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsuariosModule,
+    TareasModule,
+    ProyectosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
