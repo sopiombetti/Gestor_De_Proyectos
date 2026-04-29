965fe64a1a86bae99af87f7046482ac3a096a968
Minera José Luis <minerajl@debian>
2026-04-28 21:59:18 -0300

    Se modifica entidad para quitar relaciones que aùn no existen

--- backend/src/usuarios/entities/usuario.entity.ts
+++ backend/src/usuarios/entities/usuario.entity.ts
@@ -2,7 +2,7 @@ import { Proyecto } from 'src/proyectos/entities/proyecto.entity';
 import { Tarea } from 'src/tareas/entities/tarea.entity';
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 
-@Entity('Usuarios')
+@Entity('usuarios')
 export class Usuario {
     @PrimaryGeneratedColumn()
     id!: number;
@@ -21,11 +21,4 @@ export class Usuario {
 
     @Column()
     posicion_laboral!: string;
-
-    @OneToMany(() => Tarea, (tarea) => tarea.usuario)
-    tareas!: Tarea[];
-
-    @OneToMany(() => Proyecto, (proyecto) => proyecto.lider)
-    proyectos!: Proyecto[];
-
 }
