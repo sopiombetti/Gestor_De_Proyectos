73f5ad96fb1fb1d77770ff89944954087ec8869b
Diana2754 <dianacc.alali@gmail.com>
2026-05-02 16:22:26 -0300

    fix var

--- frontend/components/RegisterForm.jsx
+++ frontend/components/RegisterForm.jsx
@@ -71,9 +71,9 @@ export default function UserRegister(){
             <div className="w-1/2">
             <h2 className="whitespace-nowrap">Confirme contraseña</h2>
             <input class="border border-gray-500 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            type="re-contrasenia"
+            type="reContrasenia"
             placeholder="🔒 Repetir contraseña"
-            value={contrasenia}
+            value={reContrasenia}
             onChange={(e)=> setReContrasenia(e.target.value)}
             />
            
