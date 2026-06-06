c349690c3a6d89ef3d3e03801e4801bc36300b33
sopiombetti <sopiombetti@gmail.com>
2026-06-06 15:50:05 -0300

    auth admin en reporte

--- backend/src/pdf/pdf.controller.ts
+++ backend/src/pdf/pdf.controller.ts
@@ -4,16 +4,18 @@ import express from 'express';
 import { PdfService } from './pdf.service';
 import { ProyectosService } from '../proyectos/proyectos.service';
 import { AdminGuard } from '../auth/admin.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('reportes')
-//@UseGuards(AdminGuard)
+@UseGuards(JwtAuthGuard)
 export class PdfController {
   constructor(
     private readonly pdfService: PdfService,
     private readonly projectsService: ProyectosService,
   ) {}
-
+  
   @Get('proyectos/:id')
+  @UseGuards(AdminGuard)
   async getProjectReport(@Param('id') id: string, @Res() res: express.Response) {
     const project = await this.projectsService.getReportData(+id);
     const buffer  = await this.pdfService.generateProjectReport(project);
