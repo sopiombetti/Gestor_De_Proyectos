6de93699e20c3a85d6936acac29bd124d4b7b1ac
Diana Alali <dianacc.alali@gmail.com>
2026-06-09 17:57:51 -0300

    add page task-detail

---
+++ frontend/pages/tasks/task-detail.tsx
@@ -0,0 +1,11 @@
+import TaskDetail from "@/components/TaskDetail";
+
+export default function Page() {
+  return (
+    <TaskDetail
+    id={3}
+      title="Mi tarea"
+      description="Descripción de la tarea"
+    />
+  );
+}
