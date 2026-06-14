a3b73499c0025ae8dad240e81e3fcdbc0cac50d9
frannuelg <franciscogarcete12@gmail.com>
2026-06-14 09:34:11 -0300

    Documentación de apis SWAGGER.

--- backend/src/app.controller.spec.ts
+++
@@ -1,9 +0,0 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-describe('AppController', () => {
-  it('getHello devuelve el saludo del AppService', () => {
-    const controller = new AppController(new AppService());
-    expect(controller.getHello()).toBe('Hello World!');
-  });
-});
